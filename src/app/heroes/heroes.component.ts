9ac84bf945f5b2840789a8fccba1ff2501378640
pixelstroke <shonpt@live.com>
2021-09-16 22:02:51 -0800

    implemented list display with detail view

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 // Always annotate Component symbol with '@'
 // The @Component decorator function specifies Angular metadata.
@@ -9,13 +10,22 @@ import { Hero } from '../hero';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
   hero: Hero = {
     id: 1,
     name: 'Michael Scott'
   }
+  // Mock data
+  heroes = HEROES;
+
   constructor() { }
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   ngOnInit(): void {
   }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Jim' },
+    { id: 12, name: 'Pam' },
+    { id: 13, name: 'Dwight' },
+    { id: 14, name: 'Stanley' },
+    { id: 15, name: 'Meredith' },
+    { id: 16, name: 'Kelly' },
+    { id: 17, name: 'Phyllis' },
+    { id: 18, name: 'Darryl' },
+    { id: 19, name: 'Ryan'},
+    { id: 20, name: 'Angela' }
+];
